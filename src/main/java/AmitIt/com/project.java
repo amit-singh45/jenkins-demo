17080906d548629ff6ff3e536c72b14eb6ead66b
Amit <amitjee179@gmail.com>
2026-09-07 18:28:09 +0530

    Test Jenkins webhook

--- src/main/java/AmitIt/com/project.java
+++ src/main/java/AmitIt/com/project.java
@@ -3,5 +3,6 @@ package AmitIt.com;
 public class project {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins!");
+        System.out.println("Hello Jenkins - Webhook Working!");
     }
 }
