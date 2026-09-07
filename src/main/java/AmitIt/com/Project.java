8c19c55ec280078bc5107f992719681c8dc82b05
Amit <amitjee179@gmail.com>
2026-09-08 01:21:19 +0530

    ci ci cd updated

--- src/main/java/AmitIt/com/Project.java
+++ src/main/java/AmitIt/com/Project.java
@@ -1,8 +1,11 @@
 package AmitIt.com;
 
+
 public class Project {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins!");
         System.out.println("Hello Jenkins - Webhook Working!");
+        System.out.println("Hello Jenkins - CI/CD Working!");
+
     }
 }
