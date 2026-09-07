d78ed4b56514a3a89566756a60f6ddaca7e5d19d
Amit <amitjee179@gmail.com>
2026-09-08 00:41:51 +0530

    projet file update

--- src/main/java/AmitIt/com/Project.java
+++ src/main/java/AmitIt/com/Project.java
@@ -1,6 +1,6 @@
 package AmitIt.com;
 
-public class project {
+public class Project {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins!");
         System.out.println("Hello Jenkins - Webhook Working!");
